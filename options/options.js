93f8445c6ea64a38c03d191c7cbd2c359d7eaef1
Crowdbotics <team@crowdbotics.com>
2022-12-19 21:26:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tooth_sense_37940",
+  url: "https://tooth_sense_37940.botics.co",
+  api: "https://tooth_sense_37940.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
